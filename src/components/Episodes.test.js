088484855faaf284dd926ef22cf31c1b55a34984
Samantha Gatt <smfgatt@gmail.com>
2020-04-29 12:20:34 -0400

    Finishes Episodes component tests

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -12,3 +12,21 @@ test("Episodes component is set up correctly", () => {
   getByTestId(EPISODES_PARENT_DIV);
   expect(queryAllByTestId(EPISODE_DIV)).toHaveLength(mockSeasonEpisodesData.length);
 });
+
+test("Episodes component should be dynamic and rerender correct episodes", () => {
+  // Render with no episodes
+  const { getByTestId, queryAllByTestId, rerender } = render(<Episodes episodes={[]} />);
+
+  // Parent div should be present
+  getByTestId(EPISODES_PARENT_DIV);
+  // No episode divs should be in parent
+  expect(queryAllByTestId(EPISODE_DIV)).toHaveLength(0);
+
+  // Rerender with mock episodes data
+  rerender(<Episodes episodes={mockSeasonEpisodesData} />);
+  // Parent div should be present
+  getByTestId(EPISODES_PARENT_DIV);
+  // Number of episode divs should equal
+  // the number of episodes in the mock data
+  expect(queryAllByTestId(EPISODE_DIV)).toHaveLength(mockSeasonEpisodesData.length);
+});
